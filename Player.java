4e024d37ea6e5be72ee46eeedae0056c49adc872
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-06 10:27:07 -0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,63 @@
 
+package Game;
+public class Player {
+	private int hp=100;
+	private int level=1;
+	private int dmg;
+	private double physRes = 1;
+	private double magRes=1;
+
+  //object declaration
+  Misc m = new Misc();
+	
+	//Accessor Methods
+	public int getHP() {
+		return hp;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public int getDMG() {
+		return dmg;
+	}
+	//Mutator methods
+	public void setHP(int init) {
+		hp=init;
+	}
+	public void setLevel(int init) {
+		level=init;
+	}
+	public void setDMG(int init) {
+		dmg=init;
+	}
+	
+	//Level Up Methods
+	public void levelUp(){
+  }
+	/*
+	 * Moveset
+	 * Slash
+	 * Stab
+	 * Spells
+	 *Resistances
+	 *
+	 */
+	
+	public int slash() {
+    int dmg = 50;
+		boolean isCrit = m.percentRoller(20);
+    if(isCrit){
+      dmg*=2;
+    }
+    return dmg;
+	}
+	public int stab() {
+		int dmg = 40;
+		boolean isCrit = m.percentRoller(50);
+    if(isCrit){
+      dmg*=3;
+    }
+    return dmg;
+	}
+	
+}
