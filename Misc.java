d18de8490e10649346e63558bd2ad6d262eda3ed
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-06 10:29:53 -0800

    Update Misc.java

--- Misc.java
+++ Misc.java
@@ -1,9 +1,16 @@
 package Game;
 public class Misc{
-
-  public boolean percentRoller(int chance) {
+	private Misc m = new Misc();
+	public boolean percentRoller(int chance) {
 		int roll = (int) (Math.random()*100)+1;
 		return roll<=chance;
+	}
+	public int crit(int chance, int multiplier){
+    		if(m.percentRoller(chance)){
+      			return multiplier;
+    		} else {
+			return 1;
+		}
 	}
   
 }
