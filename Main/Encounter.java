42a5a8c71249012660dba2d1cc67c06df27c8add
sidisgoodgreat <98197031+sidisgoodgreat@users.noreply.github.com>
2023-11-08 08:57:57 -0800

    Update Encounter.java

--- Main/Encounter.java
+++ Main/Encounter.java
@@ -1,8 +1,15 @@
 package Main;
+package csaProject;
 import java.util.Random;
 private int Encounter_Type;
-private int area; //we import from overworld.
+//we import from overworld.
 public class Encounter {
-    int max = 5;
-    Encounter_Type = rand.nextInt(max);
+    public Encounter(String area){
+        Random rand = new Random();
+        this.Encounter_Type = Encounter_Type;
+        Encounter_Type = rand.nextInt(5);
+        this.area = area;
+        if (Encounter_Type == 1 && area == "Grassland") {
+            
+    }
 }
