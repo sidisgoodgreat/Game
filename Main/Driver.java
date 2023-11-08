f755ff1a69f3f6c8e15ef604869e8637d2581319
Epickthan <124531245+Epickthan@users.noreply.github.com>
2023-11-07 22:20:16 -0800

    fixes??
    interesting

--- Main/Driver.java
+++ Main/Driver.java
@@ -1,5 +1,42 @@
 package Main;
 
-public class Driver {
+import java.util.Scanner;
+import java.util.Random;
 
+public class Driver {
+	public static void main(String args[]) {
+			
+			Scanner sc = new Scanner(System.in);
+			Overworld main = new Overworld();
+			
+			System.out.print("Enter your name!: ");
+			
+			String name = sc.nextLine();
+			Player player1 = new Player(name);
+			
+			
+			while (main.getStep() < 100) {
+				
+				main.promptUser(player1.getLevel(), main.navigateArea( main.getStep() ) );
+				
+				if (sc.next() == "^") {
+					main.addStep();
+				} else if (sc.next() == "v") {
+					main.subStep();
+				}
+	
+				// add rando chance of fight and all that stuff here
+				// if lost/won... get back here to this class!
+				
+				// if you lose you restart hahahaha
+				if (player1.getHP() <= 0) {
+					main.setStep(0);
+					System.out.println("You hurriedly retreat back to your house after a tragic defeat!");
+					System.out.println("Time to come back stronger...");
+				}
+				
+				// to do: add an ending!!! and maybe an introduction cus we cool like that B )
+		
+			}
+		}
 }
--- Main/Overworld.java
+++ Main/Overworld.java
@@ -1,5 +1,66 @@
 package Main;
 
-public class Overworld {
 
+public class Overworld {
+	private String area;
+	private int step;
+	
+	public void promptUser(int lvl, String a) {
+
+		
+		System.out.println("On your quest to reach level 100...");
+		System.out.println("Currently you're level " + lvl + "and you're at " + a);
+		System.out.println("Do you wanna go forward? Or backward...?");
+		System.out.println("^ forward | backward v");
+	}
+	
+	public String navigateArea(int step) {
+		if (step < 5) {
+			area = "grasslands";
+		} else if (step > 10) {
+			area = "dark forest";
+		} else if (step > 15) {
+			area = "caves";
+		} else if (step > 20) {
+			area = "underworld";
+		} else if (step > 25) {
+			area = "$%@#$%#??";
+		}
+		
+		return area;
+	}
+	
+	public int getStep() {
+		return step;
+	}
+	
+	public int getArea() {
+		return area;
+	}
+	
+	public void setStep(int step) {
+		this.step = step;
+	}
+	
+	public void setArea(int area) {
+		this.area = area;
+	}
+	
+	
+	public void addStep() {
+		step++;
+	}
+	
+	public void subStep() {
+		step--;
+	}
+	
+	public void area1() {
+		
+	}
+	
+	public void greenLand () {
+		
+	}
 }
+
