739bb237429bc96e7e3a6056b3323d13764bbae9
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-09 12:27:36 -0800

    Update Driver.java

--- Main/Main/Driver.java
+++ Main/Main/Driver.java
@@ -18,13 +18,14 @@ public class Driver {
 			while (main.getStep() <= 15) {
 				
 				String input = main.promptUser(player1.getLevel(), main.navigateArea() );
-				
+				while(!(input.equals("^")||input.equals("v"))){
+					System.out.println("Invalid, please try again");
+					input = sc.next();
+				}
 				if (input.equals( "^")) {
 					main.addStep();
 				} else if (input.equals("v")&&main.getStep() > 0) {
 					main.subStep();
-				} else {
-					//Did not input eihter of them, then need to redo the choose cycle?
 				}
 	
 				// add rando chance of fight and all that stuff here
