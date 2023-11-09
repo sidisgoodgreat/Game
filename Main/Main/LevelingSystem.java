80dba81ccf221366473d227ac98d92f6d3e26f0c
sidisgoodgreat <98197031+sidisgoodgreat@users.noreply.github.com>
2023-11-08 23:16:01 -0800

    Create LevelingSystem.java

---
+++ Main/Main/LevelingSystem.java
@@ -0,0 +1,18 @@
+package main;
+import java.util.Scanner;
+public class LevelingSystem {
+  Player player = new Player();
+  public void levelUp() {
+    Scanner scan = new Scanner(System.in);
+    //placehold for xp
+    int xp;
+    int level = player.getLevel();
+
+    if(level < 21) {
+      if(xp >= 100) {
+        player.resChange();
+      }
+    }
+  }
+  }
+}
