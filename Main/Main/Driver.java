629ef8592a499efd6217c649d15edfd4dff8d928
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-11 19:27:36 -0800

    Update Driver.java

--- Main/Main/Driver.java
+++ Main/Main/Driver.java
@@ -5,49 +5,8 @@ import java.util.Random;
 
 public class Driver {
 	public static void main(String args[]) {
-			
-			Scanner sc = new Scanner(System.in);
 			Overworld main = new Overworld();
-			
-			System.out.print("Enter your name!: ");
-			
-			String name = sc.nextLine();
-			Player player1 = new Player(name);
-			
-			
-			while (main.getStep() <= 15) {
-				
-				String input = main.promptUser(player1.getLevel(), main.navigateArea() );
-				while(!(input.equals("^")||input.equals("v"))){
-					System.out.println("Invalid, please try again");
-					input = sc.next();
-				}
-				if (input.equals( "^")) {
-					main.addStep();
-				} else if (input.equals("v")&&main.getStep() > 0) {
-					main.subStep();
-				}
-	
-				// add rando chance of fight and all that stuff here
-				if (main.getStep() > 0) {
-					main.rolling();
-					main.navigateArea();
-				} else {
-					System.out.println("\nYou cannot go back any further!");
-				}
-
-				// if lost/won... get back here to this class!
-				// if you lose you restart hahahaha
-				if (player1.getHP() <= 0) {
-					main.setStep(0);
-					System.out.println("You hurriedly retreat back to your house after a tragic defeat!");
-					System.out.println("Time to come back stronger...");
-				}
-				
-				// to do: add an ending!!! and maybe an introduction cus we cool like that B )
-		
-			}
-			
+			main.game();
 			System.out.println("\nSo the big boss was yourself...");
 			System.out.println("Sounds reasonable!");
 			System.out.println("You glance back to the world you once journeyed so hard across...");
