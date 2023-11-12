3f9d8eb1ccadfad821553edc80679f04f987786e
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-11 23:44:33 -0800

    Update Driver.java
    Updating Driver to make sure it works with Overworld correctly

--- Main/Main/Driver.java
+++ Main/Main/Driver.java
@@ -6,45 +6,7 @@ import java.util.Random;
 public class Driver {
 	public static void main(String args[]) {
 			Overworld main = new Overworld();
-			Scanner sc = new Scanner(System.in);
-			
-			System.out.print("Enter your name!: ");
-			
-			String name = sc.nextLine();
-			Player player1 = new Player(name);
-			
-			
-			while (main.getStep() <= 15) {
-				
-				String input = main.promptUser(player1.getLevel(), main.navigateArea() );
-				
-				if (input.equals( "^")) {
-					main.addStep();
-				} else if (input.equals("v")&&main.getStep() > 0) {
-					main.subStep();
-				} else {
-					//Did not input eihter of them, then need to redo the choose cycle?
-				}
-	
-				// add rando chance of fight and all that stuff here
-				if (main.getStep() > 0) {
-					main.rolling();
-					main.navigateArea();
-				} else {
-					System.out.println("\nYou cannot go back any further!");
-				}
-
-				// if lost/won... get back here to this class!
-				// if you lose you restart hahahaha
-				if (player1.getHP() <= 0) {
-					main.setStep(0);
-					System.out.println("You hurriedly retreat back to your house after a tragic defeat!");
-					System.out.println("Time to come back stronger...");
-				}
-				
-		
-			}
-			
+			main.game();
 			// Peak ending ik ik i love cliches
 			System.out.println("\nSo the big final boss was yourself...");
 			System.out.println("Wow! What a twist!");
