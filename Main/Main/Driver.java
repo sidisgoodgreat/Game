939985c296e3fa910c6cfdc8b6c77019944a22df
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-11 23:44:51 -0800

    Update Driver.java

--- Main/Main/Driver.java
+++ Main/Main/Driver.java
@@ -8,16 +8,5 @@ public class Driver {
 			Overworld main = new Overworld();
 			main.game();
 			// Peak ending ik ik i love cliches
-			System.out.println("\nSo the big final boss was yourself...");
-			System.out.println("Wow! What a twist!");
-			main.game();
-			System.out.println("\nSo the big boss was yourself...");
-			System.out.println("Sounds reasonable!");
-
-			System.out.println("You glance back to the world you once journeyed so hard across...");
-			System.out.println("It is in ruins.");
-			System.out.println("Because of you.");
-			System.out.println("Maybe losing yourself in the underworld was a bad idea after all!\n");
-			System.out.println("-- END --");
 		}
 }
