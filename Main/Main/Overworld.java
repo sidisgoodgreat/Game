eab0e3088fe27c94e613f23f67d1d2ee76d3049d
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2023-11-13 10:35:25 -0800

    Update Overworld.java
    Added javadoc to (i think) entire overworld class

--- Main/Main/Overworld.java
+++ Main/Main/Overworld.java
@@ -26,13 +26,20 @@ public class Overworld {
 			}
 		return movement;
 	}
+	/**
+	* Asks the player for a name, and initilizes the Player object with this name as an argument
+	*/
 	public void begin() {
 		System.out.println("Welcome to the game!");
 		System.out.print("Enter your name: ");
 		String name = sc.nextLine();
 		player1=new Player(name);
 	}
-	
+	/**
+	*Main game cycle, Prompts user for movement using promptUser method, then rolls for a fight using 
+	*CombatEncounter, Player, and Enemy objects
+	*At the end, runs another CombatEncounter with a bossEnemy
+	*/
 	public void game() {
 		begin();
 		boolean gameWon = false;
@@ -96,13 +103,19 @@ public class Overworld {
 			System.out.println("Maybe losing yourself in the underworld was a bad idea after all!\n");
 			System.out.println("-- END --");
 	}
-
+	/**
+	* In event of losing a fight, the step variable is set to 0, otherwise returning player to the start of the game
+	* and prints some flavor text
+	*/
 	public void lostFight(){
 			setStep(0);
 			System.out.println("You hurriedly retreat back to your house after a tragic defeat!");
 			System.out.println("Time to come back stronger...");
 	}
-	
+	/**
+	* Uses global variable step and returns a String value based on what area the player should be in based on step
+	* @return - area that the player is in based on variable step, Grasslands, Desert, or Underworld.
+	*/
 	public String navigateArea() {
 		if (step < 5) {
 			setArea("Grasslands");
@@ -114,28 +127,41 @@ public class Overworld {
 		
 		return area;
 	}
-	
+	/**
+	*@return int variable step
+	*/
 	public int getStep() {
 		return step;
 	}
-	
+	/**
+	*@return String variable area
+	*/
 	public String getArea() {
 		return area;
 	}
-	
+	/**
+	* sets value of step to new value
+ 	* @param step - new value that global variable this.step will be initialized to
+	*/
 	public void setStep(int step) {
 		this.step = step;
 	}
-	
+	/**
+	*sets value of area to new value
+ 	* @param area - new value global variable area is initialized to.
+	*/
 	public void setArea(String area) {
 		this.area = area;
 	}
-	
-	
+	/**
+	* Adds 1 to the variable step
+	*/
 	public void addStep() {
 		step++;
 	}
-	
+	/**
+	* removes 1 from the variable step
+	*/
 	public void subStep() {
 		step--;
 	}	
